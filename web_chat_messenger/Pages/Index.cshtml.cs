8375674d018d5b983db3845d71eecb2ad3cac96e
MATEUS OLIVEIRA BRITO <moliveirabrito.institucional@gmail.com>
2022-08-01 09:49:00 -0300

    Cria solução e estrutura inicial do projeto

---
+++ web_chat_messenger/Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_chat_messenger.Pages {
+    public class IndexModel : PageModel {
+        public void OnGet() {
+
+        }
+    }
+}
---
+++ web_chat_messenger/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_chat_messenger.Pages {
+    public class PrivacyModel : PageModel {
+        public void OnGet() {
+        }
+    }
+}
