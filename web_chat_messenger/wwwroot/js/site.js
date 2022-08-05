f256fb78cba39ec46d13d3799a44468372df843e
MATEUS OLIVEIRA BRITO <moliveirabrito.institucional@gmail.com>
2022-08-05 11:27:37 -0300

    Implementa métodos de conexão e reconexão automática com o hub do singalR

--- web_chat_messenger/wwwroot/js/site.js
+++ web_chat_messenger/wwwroot/js/site.js
@@ -1,4 +1,17 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//Conexão e reconexão com o Hub do SignalR
+var connection = new signalR.HubConnectionBuilder().withUrl("https://localhost:5001/ChatMessengerHub").build();
 
-// Write your JavaScript code.
+connectionStart();
+
+function connectionStart() {
+    connection.start().then(function () {
+        console.info("SignalR conectado!");
+    }).catch(function (erro) {
+        console.info("Erro ao conectar com o SignalR: " + erro.toString());
+        setTimeout(connectionStart(), 5000);
+    });
+}
+
+connection.onclose(async () => {
+    await connectionStart();
+});
