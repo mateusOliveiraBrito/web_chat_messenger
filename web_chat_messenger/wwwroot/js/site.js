f2367fb0804749c5f43ea158057f1493b017bdbc
MATEUS OLIVEIRA BRITO <moliveirabrito.institucional@gmail.com>
2022-08-09 12:06:13 -0300

    Implementa métodos referentes à tela de cadastro

--- web_chat_messenger/wwwroot/js/site.js
+++ web_chat_messenger/wwwroot/js/site.js
@@ -5,6 +5,7 @@ connectionStart();
 
 function connectionStart() {
     connection.start().then(function () {
+        habilitarCadastro();
         console.info("SignalR conectado!");
     }).catch(function (erro) {
         console.info("Erro ao conectar com o SignalR: " + erro.toString());
@@ -15,3 +16,42 @@ function connectionStart() {
 connection.onclose(async () => {
     await connectionStart();
 });
+
+//Tela de cadastro
+function habilitarCadastro() {
+    var formCadastro = document.getElementById("formCadastro");
+
+    if (formCadastro != null) {
+        var btnCadastrar = document.getElementById("btnCadastrar");
+        btnCadastrar.addEventListener("click", function () {
+
+            var nome = document.getElementById("nome").value;
+            var email = document.getElementById("email").value;
+            var senha = document.getElementById("senha").value;
+
+            var usuario = {
+                Nome: nome,
+                Email: email,
+                Senha: senha
+            };
+
+            connection.invoke("CadastrarUsuario", usuario);
+        });
+    }
+
+    connection.on("ConfirmarCadastro", function (sucesso, usuario, msg) {
+        var mensagem = document.getElementById("campo-mensagem");
+        mensagem.innerText = msg;
+
+        if (sucesso) {
+            console.info(usuario);
+            limparFormularioDeCadastro();
+        }
+    });
+}
+
+function limparFormularioDeCadastro() {
+    var nome = document.getElementById("nome").value = "";
+    var email = document.getElementById("email").value = "";
+    var senha = document.getElementById("senha").value = "";
+}
