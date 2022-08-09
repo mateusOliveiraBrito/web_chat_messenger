399b2a4e149fa445c9571717b31e56dc266cae2c
MATEUS OLIVEIRA BRITO <moliveirabrito.institucional@gmail.com>
2022-08-09 15:03:07 -0300

    Implementa métodos referentes à página de login

--- web_chat_messenger/wwwroot/js/site.js
+++ web_chat_messenger/wwwroot/js/site.js
@@ -6,6 +6,8 @@ connectionStart();
 function connectionStart() {
     connection.start().then(function () {
         habilitarCadastro();
+        habilitarLogin();
+
         console.info("SignalR conectado!");
     }).catch(function (erro) {
         console.info("Erro ao conectar com o SignalR: " + erro.toString());
@@ -17,7 +19,7 @@ connection.onclose(async () => {
     await connectionStart();
 });
 
-//Tela de cadastro
+//Página de cadastro
 function habilitarCadastro() {
     var formCadastro = document.getElementById("formCadastro");
 
@@ -54,4 +56,39 @@ function limparFormularioDeCadastro() {
     var nome = document.getElementById("nome").value = "";
     var email = document.getElementById("email").value = "";
     var senha = document.getElementById("senha").value = "";
+}
+
+//Página de login
+function habilitarLogin() {
+    var formLogin = document.getElementById("formLogin");
+
+    if (formLogin != null) {
+        var btnAcessar = document.getElementById("btnAcessar").addEventListener("click", function () {
+            var email = document.getElementById("email").value;
+            var senha = document.getElementById("senha").value;
+
+            var usuario = { Email: email, Senha: senha };
+
+            connection.invoke("RealizarLogin", usuario);
+        });
+    }
+
+    connection.on("ConfirmarLogin", function (sucesso, usuarioLogado, msg) {
+
+        if (sucesso) {
+            SetUsuarioLogado(usuarioLogado);
+            window.location.href = "/Home/Conversacao";
+        } else {
+            var mensagem = document.getElementById("mensagem");
+            mensagem.innerText = msg;
+        }
+    });
+}
+
+function GetUsuarioLogado() {
+    return JSON.parse(sessionStorage.getItem("Logado"));
+}
+
+function SetUsuarioLogado(usuarioLogado) {
+    sessionStorage.setItem("Logado", JSON.stringify(usuarioLogado));
 }
